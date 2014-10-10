e302e0506a6b3be7719396005111b68e97714eef
Matthew McCall <matt@mattmccall.com>
2014-10-10 11:11:43 -0700

    Initial Commit

---
+++ gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express-server');
+
+    grunt.registerTask('default', ['jshint', 'express:dev', 'mochaTest']);
+
+    // Print a timestamp (useful for when watching)
+    grunt.registerTask('timestamp', function() {
+        grunt.log.subhead(Date());
+    });
+
+    grunt.initConfig({
+        jshint: {
+            files: ['./test/unit/*.js'], //['gruntFile.js', 'package.json', '*.js', './lib/*.js','./lib/**/*.js','./test/*.js', './test/**/*.js'],
+            options: {
+                browser: true,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: false,
+                boss: true,
+                eqnull: true,
+                node: true,
+                expr: true,
+                globals: {
+                    'xit': true,
+                    'xdescribe': true,
+                    'it': true,
+                    'describe': true,
+                    'before': true,
+                    'after': true,
+                    'done': true
+                }
+            }
+        },
+        express: {
+            dev: {
+                options: {
+                    script: './server.js'
+                }
+            }
+        },
+        watch: {
+            all: {
+                files: ['./lib/**/index.js', 'config.js', 'gruntFile.js', './models/*.js'],
+                tasks: ['default']
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    timeout: '10000'
+                },
+                src: ['test/unit/*.js', 'test/e2e/**/*.js', 'test/e2e/*.js']
+            }
+        }
+    });
+
+};
